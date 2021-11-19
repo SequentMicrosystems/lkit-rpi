73675ab01b1e2321ad340f707915d80428146fbb
alexburcea2877 <olcitu@gmail.com>
2021-11-19 22:17:35 +0000

    Add suport for servo control, firmware for hardware 2.0

--- src/lkit.c
+++ src/lkit.c
@@ -18,12 +18,13 @@
 
 #define VERSION_BASE	(int)1
 #define VERSION_MAJOR	(int)0
-#define VERSION_MINOR	(int)0
+#define VERSION_MINOR	(int)1
 
 #define UNUSED(X) (void)X      /* To avoid gcc/g++ warnings */
 #define IN_CH_NR	4
 #define RELAY_NR 	2
 #define CMD_ARRAY_SIZE	8
+#define SCALE_FACTOR 1000
 
 char *warranty =
 	"	       Copyright (c) 2016-2021 Sequent Microsystems\n"
@@ -52,6 +53,14 @@ const int gRelayPins[RELAY_NR] =
 	19,
 	16};
 
+typedef struct {
+	float min;
+	float max;
+}	CalibLimitsStruct;
+
+const CalibLimitsStruct calLimits[CALIB_CH_COUNT] = {{0, 0}, {0, 10}, {4, 20}, {4, 20}, {0, 10}, {4, 20}};
+int gServoEnable = 0;
+
 void usage(void)
 {
 	int i = 0;
@@ -73,6 +82,56 @@ void usage(void)
 	printf("Type lkit -h <command> for more help\n");
 }
 
+int writeU16(int dev, int add, int val)
+{
+	u8 buff[2];
+	u16 v = 0;
+
+	if(add < 0 || add > SLAVE_BUFF_SIZE)
+	{
+		return ARG_ERR;
+	}
+	v = (u16)val;
+	memcpy(buff, &v, 2);
+
+	return i2cMem8Write(dev, add, buff, 2);
+}
+
+int calib(int dev, float val, u8 channel, u8 reset)
+{
+	int rawVal = 0;
+	u8 buff[2];
+	int ret = 0;
+
+	if( channel <= CALIB_CH_NONE || channel >= CALIB_CH_COUNT)
+	{
+		printf("Invalid calibration channel %d \n", (int)channel);
+		return ARG_ERR;
+	}
+
+	if(reset)
+	{
+		buff[0] = channel;
+		buff[1] = RESET_CALIBRATION_KEY;
+		return i2cMem8Write(dev, I2C_MEM_CALIB_CHANNEL, buff, 2);
+	}
+
+	if( val < calLimits[channel].min || val > calLimits[channel].max)
+	{
+		printf("Invalid calibration value, most be [%f, %f]!\n",calLimits[channel].min, calLimits[channel].max);
+		return ARG_ERR;
+	}
+	rawVal = (int)round(val * SCALE_FACTOR);
+	ret = writeU16(dev, I2C_MEM_CALIB_VALUE, rawVal);
+	if (ret)
+	{
+		return ret;
+	}
+	buff[0] = channel;
+	buff[1] = CALIBRATION_KEY;
+	return i2cMem8Write(dev, I2C_MEM_CALIB_CHANNEL, buff, 2);
+}
+
 int doBoardInit(void)
 {
 	int dev = 0;
@@ -85,11 +144,12 @@ int doBoardInit(void)
 	{
 		return ERROR;
 	}
-	if (ERROR == i2cMem8Read(dev, I2C_MEM_REVISION_MAJOR_ADD, buff, 1))
+	if (ERROR == i2cMem8Read(dev, I2C_MEM_SERVO_ENABLE, buff, 1))
 	{
 		printf("Learning Kit card not detected!\n");
 		return ERROR;
 	}
+	gServoEnable = buff[0];
 	return dev;
 }
 
@@ -105,11 +165,12 @@ int boardCheck(void)
 	{
 		return ERROR;
 	}
-	if (ERROR == i2cMem8Read(dev, I2C_MEM_REVISION_MAJOR_ADD, buff, 1))
+	if (ERROR == i2cMem8Read(dev, I2C_MEM_SERVO_ENABLE, buff, 1))
 	{
 
 		return ERROR;
 	}
+	gServoEnable = buff[0];
 	return OK;
 }
 
@@ -475,12 +536,11 @@ const CliCmdType CMD_VOUT_WRITE =
 int doVoutWrite(int argc, char *argv[])
 {
 	int dev = -1;
-	u8 buff[2];
 	int resp = 0;
 	float value = 0;
-	u16 aux = 0;
+	int val = 0;
+	u8 buff[2];
 
-	UNUSED(argv);
 	if (argc != 3)
 	{
 		return ARG_CNT_ERR;
@@ -491,15 +551,24 @@ int doVoutWrite(int argc, char *argv[])
 	{
 		return FAIL;
 	}
+	if(0 != gServoEnable)
+	{
+		buff[0] = 0;
+		resp = i2cMem8Write(dev,I2C_MEM_SERVO_ENABLE, buff, 1);
+		if( FAIL == resp)
+		{
+			printf("Fail to write!\n");
+			return FAIL;
+		}
+	}
 	value = atof(argv[2]);
-	if (value > 10 || value < 0)
+	if( value > 10 || value < 0)
 	{
 		return ARG_ERR;
 	}
-	aux = (u16)round(value * 1000);
-	memcpy(buff, &aux, 2);
-	resp = i2cMem8Write(dev, I2C_MEM_V_OUT_ADD, buff, 2);
-	if (FAIL == resp)
+	val = (u16)round(value * 1000);
+	resp = writeU16(dev, I2C_MEM_V_OUT_ADD, val);
+	if (OK != resp)
 	{
 		printf("Fail to write!\n");
 		return FAIL;
@@ -545,6 +614,67 @@ int doIinRead(int argc, char *argv[])
 	memcpy(&resp, buff, 2);
 	printf("%.3f\n", (float)resp / 1000);
 	return OK;
+}
+
+
+int doIinCal(int argc, char *argv[]);
+const CliCmdType CMD_IIN_CAL =
+{
+	"iincal",
+	1,
+	&doIinCal,
+	"\tiincal		Calibrate 4-20mA input port\n",
+	"\tUsage:		lkit iincal <val>\n",
+	"",
+	"\tExample:		lkit iincal 4.23  Calibrate the 4-20mA input port at 4.23mA\n"};
+
+int doIinCal(int argc, char *argv[])
+{
+	int dev = -1;
+	float value = 0;
+
+	if (argc != 3)
+	{
+		return ARG_CNT_ERR;
+	}
+
+	dev = doBoardInit();
+	if (dev <= 0)
+	{
+		return FAIL;
+	}
+	value = atof(argv[2]);
+	return calib(dev, value, CALIB_CH_I_IN, 0);
+}
+
+int doIinCalRst(int argc, char *argv[]);
+const CliCmdType CMD_IIN_CAL_RST =
+{
+	"iincalrst",
+	1,
+	&doIinCalRst,
+	"\tiincalrst		Reset the 4-20mA input port calibration\n",
+	"\tUsage:		lkit iincalrst \n",
+	"",
+	"\tExample:		lkit iincalrst Reset the 4-20mA input port calibration\n"};
+
+int doIinCalRst(int argc, char *argv[])
+{
+	int dev = -1;
+	
+	UNUSED(argv);
+	if (argc != 2)
+	{
+		return ARG_CNT_ERR;
+	}
+
+	dev = doBoardInit();
+	if (dev <= 0)
+	{
+		return FAIL;
+	}
+	
+	return calib(dev, 0, CALIB_CH_I_IN, 1);
 }
 
 int doIoutRead(int argc, char *argv[]);
@@ -605,23 +735,32 @@ int doIoutWrite(int argc, char *argv[])
 	int resp = 0;
 	float value = 0;
 	u16 aux = 0;
-
-	UNUSED(argv);
+	
 	if (argc != 3)
 	{
 		return ARG_CNT_ERR;
 	}
-
 	dev = doBoardInit();
 	if (dev <= 0)
 	{
 		return FAIL;
+	}
+	if(0 != gServoEnable)
+	{
+		buff[0] = 0;
+		resp = i2cMem8Write(dev,I2C_MEM_SERVO_ENABLE, buff, 1);
+		if( FAIL == resp)
+		{
+			printf("Fail to write!\n");
+			return FAIL;
+		}
 	}
 	value = atof(argv[2]);
 	if (value > 20 || value < 4)
 	{
 		return ARG_ERR;
 	}
+
 	aux = (u16)round(value * 1000);
 	memcpy(buff, &aux, 2);
 	resp = i2cMem8Write(dev, I2C_MEM_I_OUT_ADD, buff, 2);
@@ -720,6 +859,114 @@ int doMotWrite(int argc, char *argv[])
 	return OK;
 }
 
+int doServoRead(int argc, char *argv[]);
+const CliCmdType CMD_SERVO_READ =
+{
+	"servord",
+	1,
+	&doServoRead,
+	"\tservord		Display the servo position value in %\n",
+	"\tUsage:		lkit servord <channel>\n",
+	"",
+	"\tExample:		lkit servord 1  Display the servo 1 position \n"};
+
+int doServoRead(int argc, char *argv[])
+{
+	int dev = -1;
+	u8 buff[2];
+	int resp = 0;
+	int channel = 0;
+	s16 val = 0;
+
+	UNUSED(argv);
+	if (argc != 3)
+	{
+		return ARG_CNT_ERR;
+	}
+	channel = atoi(argv[2]);
+	if(1 != channel && 2 != channel)
+	{
+		return ARG_ERR;
+	}
+
+	dev = doBoardInit();
+	if (dev <= 0)
+	{
+		return FAIL;
+	}
+	resp = i2cMem8Read(dev, I2C_MEM_SERVO_VAL1 + 2 * (channel - 1), buff, 2);
+	if (FAIL == resp)
+	{
+		printf("Fail to read!\n");
+		return FAIL;
+	}
+
+	memcpy(&val, buff, 2);
+	printf("%.1f\n", (float)val / 10);
+	return OK;
+}
+
+int doServoWrite(int argc, char *argv[]);
+const CliCmdType CMD_SERVO_WRITE =
+{
+	"servowr",
+	1,
+	&doServoWrite,
+	"\tservowr		Set the servo position (-100..100) for standard (-140..140) for extended range servo's \n",
+	"\tUsage:		lkit servowr <channel> <value(%)>\n",
+	"",
+	"\tExample:		lkit servowr 1 25.2  Set the servo 1 position to 25.2%  \n"};
+
+int doServoWrite(int argc, char *argv[])
+{
+	int dev = -1;
+	u8 buff[2];
+	int resp = 0;
+	float value = 0;
+	int channel = 0;
+	s16 aux = 0;
+
+	if (argc != 4)
+	{
+		return ARG_CNT_ERR;
+	}
+	channel = atoi(argv[2]);
+	if(1 != channel && 2 != channel)
+	{
+		return ARG_ERR;
+	}
+	value = atof(argv[3]);
+	if (value > 140 || value < -140)
+	{
+		return ARG_ERR;
+	}
+	dev = doBoardInit();
+	if (dev <= 0)
+	{
+		return FAIL;
+	}
+	if(0 == gServoEnable)
+	{
+		buff[0] = 1;
+		resp = i2cMem8Write(dev,I2C_MEM_SERVO_ENABLE, buff, 1);
+		if( FAIL == resp)
+		{
+			printf("Fail to write!\n");
+			return FAIL;
+		}
+	}
+	aux = (s16)round(value * 10);
+	memcpy(buff, &aux, 2);
+	
+	resp = i2cMem8Write(dev, I2C_MEM_SERVO_VAL1 + 2 * (channel - 1), buff, 2);
+	if (FAIL == resp)
+	{
+		printf("Fail to write!\n");
+		return FAIL;
+	}
+	return OK;
+}
+
 int doLedWrite(int argc, char *argv[]);
 const CliCmdType CMD_LED_WRITE =
 {
@@ -803,10 +1050,14 @@ const CliCmdType *gCmdArray[] =
 	&CMD_VOUT_READ,
 	&CMD_VOUT_WRITE,
 	&CMD_IIN_READ,
+	&CMD_IIN_CAL,
+	&CMD_IIN_CAL_RST,
 	&CMD_IOUT_READ,
 	&CMD_IOUT_WRITE,
 	&CMD_MOT_READ,
 	&CMD_MOT_WRITE,
+	&CMD_SERVO_READ,
+	&CMD_SERVO_WRITE,
 	&CMD_LED_WRITE,
 	NULL};
 
@@ -830,6 +1081,15 @@ int main(int argc, char *argv[])
 				if (ret == ARG_CNT_ERR)
 				{
 					printf("Invalid parameters number!\n");
+					printf("%s", gCmdArray[i]->usage1);
+					if (strlen(gCmdArray[i]->usage2) > 2)
+					{
+						printf("%s", gCmdArray[i]->usage2);
+					}
+				}
+				else if(ret == ARG_ERR)
+				{
+					printf("Invalid parameters!\n");
 					printf("%s", gCmdArray[i]->usage1);
 					if (strlen(gCmdArray[i]->usage2) > 2)
 					{
